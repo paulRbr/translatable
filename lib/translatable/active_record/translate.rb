ea526c55f09bbad70d1469403943854a3c025eb9
Paul B <paul@bonaud.fr>
2014-02-17 11:35:31 +0100

    Remove tmp files gitignore

--- lib/translatable/active_record/translate.rb
+++ lib/translatable/active_record/translate.rb
@@ -6,14 +6,19 @@ module Translatable
         model = self.table_name
         if Translatable::Config.list.has_key? model and self.column_names.include? 'group_id'
 
-          to_translate = attr_names.length == 0 ? Translatable::Config.list[model] : attr_names
-
-          q = has_select_values? ? self : self.select("#{model}.*")
+          if attr_names.length == 0
+            to_translate = Translatable::Config.list[model]
+            q = has_select_values? ? self : self.select("#{model}.*")
+          else
+            to_translate = attr_names
+            q = self
+          end
 
           index = 0
           to_translate.each do |attr|
 
-            if in_scope?
+            if is_in_scope? attr
+              p "in_scope? attr : #{is_in_scope? attr}"
               index += 1
               if Translatable::Config.list[model].include?(attr.to_s)
                 q = q.joins("LEFT JOIN snippets sn#{index} ON sn#{index}.record_id = #{model}.id AND sn#{index}.scope = '#{model}' AND sn#{index}.group_id = #{model}.group_id AND sn#{index}.lang = '#{lang}' AND sn#{index}.key = '#{attr}'").
@@ -24,12 +29,12 @@ module Translatable
             end
 
           end
-          q
+
+          q.scoped
         else
-          self
+          self.scoped
         end
 
-      end
       end
 
       private
@@ -38,18 +43,20 @@ module Translatable
         self.scoped.select_values.length > 0
       end
 
-      def in_scope?(attr)
+      def is_in_scope?(attr)
 
         if has_select_values?
           select_values = self.scoped.select_values
           select_values.select{ |value| value.is_a?(String) && value.split('.').last == '*' }.length > 0 ||
-            select_values.include?(attr) ||
-            select_values.include?(attr.to_sym) ||
-            select_values.select{ |value| value.is_a?(String) && value.split('.').last == attr }.length > 0
+              select_values.include?(attr) ||
+              select_values.include?(attr.to_sym) ||
+              select_values.select{ |value| value.is_a?(String) && value.split('.').last == attr }.length > 0
         else
           true
         end
 
       end
+
+    end
   end
 end
